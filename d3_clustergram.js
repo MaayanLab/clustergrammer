04890edbbff65896b0b5ce6a27d2d15c3253ea18
cornhundred <doctorcorn2002@gmail.com>
2015-10-26 18:10:23 -0400

    grid lines working

--- d3_clustergram.js
+++ d3_clustergram.js
@@ -3972,6 +3972,45 @@ function enter_exit_update(params, network_data, delays){
   var tmp_dendrogram = Dendrogram('row', params, row_triangle_ini_group, duration);
   var tmp_dendrogram = Dendrogram('col', params, row_triangle_ini_group, duration);
 
+
+    // append horizontal lines
+    d3.select('#clust_group')
+      .selectAll('.horz_lines')
+      .data(row_nodes, function(d){return d.name;})
+      .enter()
+      .append('g')
+      .attr('class','horz_lines')
+      .attr('transform', function(d, index) {
+        return 'translate(0,' + params.matrix.y_scale(index) + ') rotate(0)';
+      })
+      .append('line')
+      .attr('x1',0)
+      .attr('x2',params.viz.clust.dim.width)
+      .style('stroke-width', params.viz.border_width/params.viz.zoom_switch+'px')
+      .style('stroke','white')
+      .attr('opacity',0)
+      .transition().delay(delays.enter).duration(duration)
+      .attr('opacity',1);
+
+    // append vertical line groups
+    d3.select('#clust_group')
+      .selectAll('.vert_lines')
+      .data(col_nodes)
+      .enter()
+      .append('g')
+      .attr('class', 'vert_lines')
+      .attr('transform', function(d, index) {
+          return 'translate(' + params.matrix.x_scale(index) + ') rotate(-90)';
+      })
+      .append('line')
+      .attr('x1', 0)
+      .attr('x2', -params.viz.clust.dim.height)
+      .style('stroke-width', params.viz.border_width + 'px')
+      .style('stroke', 'white')
+      .attr('opacity',0)
+      .transition().delay(delays.enter).duration(duration)
+      .attr('opacity',1);
+
 }
 
 
