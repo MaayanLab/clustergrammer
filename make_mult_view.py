5a56681beb0a37c48a40cfcdcea2e8c269f538e3
cornhundred <doctorcorn2002@gmail.com>
2015-11-24 12:55:01 -0500

    starting to implement views, will set up crossfilter to switch between views

---
+++ make_mult_view.py
@@ -0,0 +1,59 @@
+# import network class from Network.py
+from d3_clustergram import Network
+from copy import deepcopy
+
+# set up range as function of maximum value in matrix 
+all_filt = range(10)
+
+inst_meet = 1
+
+# calc mult_view net 
+net_view = deepcopy(Network())
+net_view.load_tsv_to_net('txt/example_tsv_network.txt')
+net_view.cluster_row_and_col('cos')
+
+net_view.viz['views'] = []
+
+all_views = []
+
+for inst_filt in all_filt:
+
+  print('\ninst_filt\t'+str(inst_filt))
+
+  # load network from tsv file
+  ##############################
+  net = deepcopy(Network())
+  # net.load_tsv_to_net('txt/example_tsv_network.txt')
+
+  net.dat = deepcopy(net_view.dat)
+
+  net.filter_network_thresh(inst_filt,inst_meet)
+
+  mat_shape = net.dat['mat'].shape
+
+  try:
+    # cluster
+    #############
+    # only compare vectors with at least min_num_comp common data points
+    # with absolute values above cutoff_comp
+    net.cluster_row_and_col('cos')
+
+    # add view 
+    inst_view = {}
+    inst_view['filt'] = inst_filt
+    inst_view['num_meet'] = inst_meet
+    inst_view['nodes'] = {}
+    inst_view['nodes']['row_nodes'] = net.viz['row_nodes']
+    inst_view['nodes']['col_nodes'] = net.viz['col_nodes']
+
+    all_views.append(inst_view)
+
+  except:
+    print('\n\ndo not make clustergram\n\n')
+
+# add views to viz
+net_view.viz['views'] = all_views
+
+# export data visualization to file
+######################################
+net_view.write_json_to_file('viz', 'json/mult_view.json', 'indent')
