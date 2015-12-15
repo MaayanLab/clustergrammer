54847c98f643ee633e341666925bbc2d3df6ca63
cornhundred <doctorcorn2002@gmail.com>
2015-12-15 13:35:21 -0500

    using default example

--- make_mult_view.py
+++ make_mult_view.py
@@ -3,10 +3,9 @@ from clustergrammer import Network
 
 net = Network()
 
-# net.load_tsv_to_net('txt/example_tsv_network.txt')
-print('loading')
-net.load_tsv_to_net('txt/mat_2kb.txt')
-print('working on views')
-net.make_mult_views(dist_type='cos',filter_row=['num'])
+net.load_tsv_to_net('txt/example_tsv_network.txt')
+# net.load_tsv_to_net('txt/mat_1mb.txt')
+
+net.make_mult_views(dist_type='cos',filter_row=['sum','value','num'])
 
 net.write_json_to_file('viz', 'json/mult_view.json', 'indent')
