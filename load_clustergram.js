4303786678816ec4177242466d37c170c913c21d
cornhundred <doctorcorn2002@gmail.com>
2015-12-24 17:13:28 -0500

    switched to default viz

--- load_clustergram.js
+++ load_clustergram.js
@@ -335,12 +335,12 @@ function make_clust(inst_network){
 
 // choose example here
 // make_clust('default_example.json');
-// make_clust('mult_view.json');
+make_clust('mult_view.json');
 // make_clust('enr_clust_example.json');
 // make_clust('updn_example.json');
 // make_clust('narrow_example.json');
 // make_clust('narrow_long_name.json');
 // make_clust('bar_example.json');
-make_clust('kin_sub_example.json');
+// make_clust('kin_sub_example.json');
 // make_clust('harmonogram_example.json');
 
