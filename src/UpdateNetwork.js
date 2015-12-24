e348e1664067d77807418384d8221bfe9fcbe617
cornhundred <doctorcorn2002@gmail.com>
2015-12-24 16:14:03 -0500

    bringing updated clustergrammer.js code to clustergrammer web app

--- src/UpdateNetwork.js
+++ src/UpdateNetwork.js
@@ -234,14 +234,8 @@ function enter_exit_update(params, network_data, reorder, delays){
   // enter exit update tiles  
   function eeu_existing_row(ini_inp_row_data){
 
-    // console.log(ini_inp_row_data.row_data)
 
     var inp_row_data = ini_inp_row_data.row_data;
-
-    // _.each(inp_row_data, function(d){
-    //   console.log(d.name);
-    // })
-
 
     // remove zero values from 
     var row_values = _.filter(inp_row_data, function(num){
@@ -251,7 +245,10 @@ function enter_exit_update(params, network_data, reorder, delays){
     // bind data to tiles 
     var cur_row_tiles = d3.select(this)
       .selectAll('.tile')
-      .data(row_values, function(d){return d.col_name;});
+      .data(row_values, function(d){
+        // console.log(d.col_name);
+        return d.col_name;
+      });
 
     ///////////////////////////
     // Exit 
@@ -536,9 +533,16 @@ function enter_exit_update(params, network_data, reorder, delays){
       .attr('fill-opacity',0)
       .attr('transform', function(d){
         var x_pos = params.matrix.x_scale(d.pos_x) + 0.5*params.viz.border_width;
-        console.log(d.pos_x)
         var y_pos = 0.5*params.viz.border_width/params.viz.zoom_switch;
-        console.log(String(x_pos)+' '+String(y_pos))
+
+        // if (isNaN(x_pos)){
+        //   console.log('\n')
+        //   console.log(d.col_name)
+        //   console.log(d.pos_x)
+        //   console.log(x_pos)
+        //   console.log('\n')
+        // }
+
         return 'translate('+x_pos+','+y_pos+')';
       });
 
--- use_pandas_to_filter.py
+++ use_pandas_to_filter.py
@@ -10,58 +10,16 @@ def main():
   net = Network()
 
   # load data to dataframe 
-  net.load_tsv_to_net('txt/mat_1mb.txt')
+  net.load_tsv_to_net('txt/example_tsv_network.txt')
 
   # perform fast row filtering 
   net.fast_mult_views()
 
-  net.write_json_to_file('viz', 'json/mult_view_fast.json', 'indent')
+  net.write_json_to_file('viz', 'json/mult_view.json', 'indent')
 
   # your code
   elapsed_time = time.time() - start_time
   print('\n\n\nelapsed time: '+str(elapsed_time))
 
-  # print(df)
-
-  # print('\n\nafter filtering')
-
-  # inst_cols = ['Col-1','Col-2']
-  # df = df[inst_cols]
-
-  # df = df.abs()
-
-  # print(df)
-
-  # inst_rows = ['CDK4','EGFR']
-  # df = df.loc('EGFR')
-
-  # print(df.shape[0])
-  # print(df.shape[1])
-
-
-  # net.fast_mult_views()
-
-  # print(df)
-
-  # print('filter rows')
-
-  # # filter rows 
-  # df = df[df.sum(axis=1) > 10]
-
-  # # filter columns, first transpose 
-  # df = df.transpose()
-
-  # df = df[df.sum(axis=1) > 10]
-
-  # df = df.transpose()
-
-  # # load the data back to dat
-  # net.df_to_dat(df)
-
-  # print(net.dat)
-
-  # net.make_mult_views(dist_type='cos',filter_row=['sum'])
-
-  # net.write_json_to_file('viz', 'json/mult_view.json', 'indent')
 
 main()
