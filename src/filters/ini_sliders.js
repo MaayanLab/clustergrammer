3c39004fec1f31f74c29b33b21d95f6ff0089b69
cornhundred <doctorcorn2002@gmail.com>
2016-02-17 16:36:30 -0500

    ini_sliders using change_groups function directly

--- src/filters/ini_sliders.js
+++ src/filters/ini_sliders.js
@@ -1,3 +1,4 @@
+var change_groups = require('../dendrogram/change_groups');
 
 module.exports = function ini_sliders(cgm){
 
@@ -10,7 +11,7 @@ module.exports = function ini_sliders(cgm){
       slide: function( event, ui ) {
         $( "#amount" ).val( "$" + ui.value );
         var inst_index = ui.value*10;
-        cgm.change_groups(cgm.params, 'col',inst_index);
+        change_groups(cgm.params, 'col',inst_index);
       }
     });
     $( "#amount" ).val( "$" + $( ".slider_col" ).slider( "value" ) );
@@ -24,7 +25,7 @@ module.exports = function ini_sliders(cgm){
       slide: function( event, ui ) {
         $( "#amount" ).val( "$" + ui.value );
         var inst_index = ui.value*10;
-        cgm.change_groups(cgm.params, 'row',inst_index);
+        change_groups(cgm.params, 'row',inst_index);
       }
     });
     $( "#amount" ).val( "$" + $( ".slider_row" ).slider( "value" ) );
