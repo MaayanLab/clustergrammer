881ef816ccae53597d1e1790057e3f3fbddb7961
cornhundred <doctorcorn2002@gmail.com>
2016-02-18 15:25:04 -0500

    added back wait sign

--- load_clustergram.js
+++ load_clustergram.js
@@ -1,7 +1,6 @@
 
-make_clust('mult_view.json');
+// make_clust('mult_view.json');
 // make_clust('vect_post_example.json');
-// make_clust('large_example.json');
 // make_clust('default_example.json');
 // make_clust('ccle.json');
 // make_clust('enr_clust_example.json');
@@ -10,7 +9,7 @@ make_clust('mult_view.json');
 // make_clust('narrow_long_name.json');
 // make_clust('bar_example.json');
 // make_clust('kin_sub_example.json');
-// make_clust('harmonogram_example.json');
+make_clust('harmonogram_example.json');
 
 
 function make_clust(inst_network){
