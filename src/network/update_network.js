c1f0112097dd79facce47d13d669bcb55a332fed
cornhundred <doctorcorn2002@gmail.com>
2016-02-18 11:58:27 -0500

    removing function redefinitions from cgm in update_network

--- src/network/update_network.js
+++ src/network/update_network.js
@@ -4,25 +4,25 @@ var initialize_resizing = require('../initialize_resizing');
 var ini_doubleclick = require('../ini_doubleclick');
 var enter_exit_update = require('./enter_exit_update');
 var define_enter_exit_delays = require('./define_enter_exit_delays');
-var search = require('../search');
-var all_reorder = require('../reorder/all_reorder');
+// var search = require('../search');
+// var all_reorder = require('../reorder/all_reorder');
 var build_col_dendro = require('../dendrogram/build_col_dendro');
 var build_row_dendro = require('../dendrogram/build_row_dendro');
 var ini_sliders = require('../filters/ini_sliders');
 
-module.exports = function(cgm, old_params, change_view) {
+module.exports = function(cgm, config, old_params, change_view) {
 
   /*
   This is being run by the cgm object, and has access to config. 
   */
 
   // make new_network_data by filtering the original network data
-  var config_copy = jQuery.extend(true, {}, cgm.config);
+  var config_copy = jQuery.extend(true, {}, config);
 
   var new_network_data = change_network_view(old_params, config_copy.network_data, change_view);
 
   // make tmp config to make new params 
-  var tmp_config = jQuery.extend(true, {}, cgm.config);
+  var tmp_config = jQuery.extend(true, {}, config);
 
   tmp_config.network_data = new_network_data;
   tmp_config.inst_order = old_params.viz.inst_order;
@@ -33,18 +33,18 @@ module.exports = function(cgm, old_params, change_view) {
   var params = make_params(tmp_config);
   var delays = define_enter_exit_delays(old_params, params);
 
-  // ordering - necessary for reordering the function called on button click
-  cgm.reorder = all_reorder;
-
   enter_exit_update(params, new_network_data, delays);
 
   // update network data in params
   cgm.params = params;
 
-  // search functions
-  var gene_search = search(params, params.network_data.row_nodes, 'name');
-  cgm.get_entities = gene_search.get_entities;
-  cgm.find_entity = gene_search.find_entity;
+  // // ordering - necessary for reordering the function called on button click
+  // cgm.reorder = all_reorder;
+
+  // // search functions
+  // var search_obj = search(params, params.network_data.row_nodes, 'name');
+  // cgm.get_entities = search_obj.get_entities;
+  // cgm.find_entity = search_obj.find_entity;
 
   // TODO reenable dendrogram updating
   // redefine change_group function
