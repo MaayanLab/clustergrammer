5c7a36eefcef5b954690c5c22ad8bd8402947ac5
cornhundred <doctorcorn2002@gmail.com>
2016-02-22 16:38:05 -0500

    local version that will be used for D3-meetup

--- src/filters/set_up_filters.js
+++ src/filters/set_up_filters.js
@@ -22,19 +22,19 @@ module.exports = function set_up_filters(config, params, filter_type, initial_te
 
   var views = params.network_data.views;
 
-  var all_views = _.filter(views, function(d) { return utils.has(d,filter_type); });
+  var available_views = _.filter(views, function(d) { return utils.has(d,filter_type); });
 
   // // find views of current category 
-  // if ( utils.has(all_views[0],'col_cat') ) {
+  // if ( utils.has(available_views[0],'col_cat') ) {
   //   // get views with current_col_cat
-  //   all_views = _.filter(all_views, function(d){
+  //   available_views = _.filter(available_views, function(d){
   //     if (d.col_cat == params.current_col_cat){
   //       return d;
   //     }
   //   })
   // }
 
-  var inst_max = all_views.length - 1;
+  var inst_max = available_views.length - 1;
 
   $( '.slider_'+filter_type ).slider({
     value:0,
@@ -46,16 +46,16 @@ module.exports = function set_up_filters(config, params, filter_type, initial_te
       // get value
       var inst_index = $( params.root+' .slider_'+filter_type ).slider( "value" );
 
-      var inst_view = all_views[inst_index][filter_type];
+      var inst_view_name = available_views[inst_index][filter_type];
 
       var requested_view = {};
-      requested_view[filter_type] = inst_view;
+      requested_view[filter_type] = inst_view_name;
 
       disable_sidebar(params);
 
       params = update_network(config, params, requested_view);
 
-      d3.select(params.root+' .'+filter_type).text('Top rows: '+inst_view+' rows');
+      d3.select(params.root+' .'+filter_type).text('Top rows: '+inst_view_name+' rows');
 
       setTimeout(enable_sidebar, 2500, params);
 
