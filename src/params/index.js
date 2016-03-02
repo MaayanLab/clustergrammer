29c786e6fa57481ea5d1cb44e69f658634dc37d6
cornhundred <doctorcorn2002@gmail.com>
2016-03-02 15:01:39 -0500

    cleaning up params

--- src/params/index.js
+++ src/params/index.js
@@ -4,7 +4,7 @@ var parent_div_size = require('../parent_div_size');
 var is_force_square = require('./is_force_square');
 var get_svg_dim = require('./get_svg_dim');
 var set_label_params = require('./set_label_params');
-var set_viz_params = require('./set_viz_params');
+var ini_viz_params = require('./ini_viz_params');
 var ini_matrix_params = require('./ini_matrix_params');
 var set_clust_width = require('./set_clust_width');
 var calc_default_fs = require('./calc_default_fs');
@@ -28,13 +28,8 @@ module.exports = function make_params(input_config) {
   }
 
   params = set_label_params(config, params);
-
-  params = set_viz_params(config, params);
-
+  params = ini_viz_params(config, params);
   params = ini_matrix_params(config, params);
-
-  var col_nodes = params.network_data.col_nodes;
-  var row_nodes = params.network_data.row_nodes;
 
   // Create wrapper around SVG visualization
   d3.select(config.root).append('div').attr('class', 'viz_wrapper');
@@ -44,8 +39,8 @@ module.exports = function make_params(input_config) {
 
   params = get_svg_dim(params);
 
-  params.network_data.row_nodes_names = _.pluck(row_nodes, 'name');
-  params.network_data.col_nodes_names = _.pluck(col_nodes, 'name');
+  params.network_data.row_nodes_names = _.pluck(params.network_data.row_nodes, 'name');
+  params.network_data.col_nodes_names = _.pluck(params.network_data.col_nodes, 'name');
 
   params.norm_label.margin = {};
   params.norm_label.margin.left = params.viz.grey_border_width + params.labels.super_label_width;
@@ -77,8 +72,8 @@ module.exports = function make_params(input_config) {
   params.colorbar_room.row = tmp_colorbar_room;
   params.colorbar_room.col = tmp_colorbar_room;
 
-  params.viz.num_col_nodes = col_nodes.length;
-  params.viz.num_row_nodes = row_nodes.length;
+  params.viz.num_col_nodes = params.network_data.col_nodes.length;
+  params.viz.num_row_nodes = params.network_data.row_nodes.length;
 
   params.viz.clust.dim = {};
 
@@ -89,7 +84,7 @@ module.exports = function make_params(input_config) {
     params.viz.force_square = 1;
   }
 
-  var enr_max = Math.abs(_.max(col_nodes, function (d) {
+  var enr_max = Math.abs(_.max(params.network_data.col_nodes, function (d) {
     return Math.abs(d.value);
   }).value);
 
@@ -98,7 +93,7 @@ module.exports = function make_params(input_config) {
     .domain([0, enr_max])
     .range([0, 0.75 * params.norm_label.width.col]);
 
-  enr_max = Math.abs(_.max(row_nodes, function (d) {
+  enr_max = Math.abs(_.max(params.network_data.row_nodes, function (d) {
     return Math.abs(d.value);
   }).value);
   params.labels.bar_scale_row = d3.scale
--- src/params/ini_viz_params.js
+++ src/params/ini_viz_params.js
@@ -2,17 +2,11 @@ module.exports = function set_viz_params(config, params){
 
   params.viz = {};
 
-  params.viz.spillover_x_offset = params.norm_label.width.col;
-
   params.viz.viz_wrapper = config.root + ' .viz_wrapper';
-  params.viz.viz_svg = params.viz.viz_wrapper + ' .viz_svg';
-
   params.viz.do_zoom = config.do_zoom;
   params.viz.resize = config.resize;
-  // background colors
   params.viz.background_color = config.background_color;
   params.viz.super_border_color = config.super_border_color;
-  // margin widths
   params.viz.outer_margins = config.outer_margins;
   params.viz.outer_margins_expand = config.outer_margins_expand;
   params.viz.expand = config.ini_expand;
@@ -20,27 +14,21 @@ module.exports = function set_viz_params(config, params){
   params.viz.grey_border_width = config.grey_border_width;
   params.viz.show_dendrogram = config.show_dendrogram;
   params.viz.tile_click_hlight = config.tile_click_hlight;
-
-  params.viz.uni_duration = 1000;
-
   params.viz.inst_order = config.inst_order;
-
   params.viz.expand_button = config.expand_button;
 
+  params.viz.viz_svg = params.viz.viz_wrapper + ' .viz_svg';
+  params.viz.uni_duration = 1000;
+  params.viz.spillover_x_offset = params.norm_label.width.col;
   params.viz.bottom_space = 15;
-
   params.viz.run_trans = false;
-
   params.viz.duration = 1000;
 
   params.cat_room = {};
-
   params.cat_room.symbol_width = 11;
-
   if (params.viz.show_dendrogram) {
     params.cat_room.row = 2 * params.cat_room.symbol_width;
     params.cat_room.col = params.cat_room.symbol_width;
-
   } else {
     params.cat_room.row = params.cat_room.symbol_width;
     params.cat_room.col = 0;
