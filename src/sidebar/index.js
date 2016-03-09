84316d7ed39a3f974c9f20b9dffc0044f66c9eb0
cornhundred <doctorcorn2002@gmail.com>
2016-03-09 14:54:06 -0500

    unstable, added key_cat_col, working on category ordering

--- src/sidebar/index.js
+++ src/sidebar/index.js
@@ -55,7 +55,8 @@ module.exports = function sidebar(config, params) {
 
     // set up column category key 
     var key_cat_col = d3.select(params.root+' .sidebar_wrapper')
-      // .style('background-color','red')
+      .append('div')
+      .classed('key_cat_col',true)
       .style('margin-top','10px')
       .style('padding','5px')
       .style('border','1px solid #DEDEDE')
@@ -70,37 +71,89 @@ module.exports = function sidebar(config, params) {
 
     for (var inst_cat in params.viz.cat_colors.col){
 
-      console.log(inst_cat)
+      var inst_group = key_cat_col
+        .append('g')
+        .attr('class','category_section')
+        .on('click', category_key_click);
 
-      // var inst_group = key_cat_col
-      //   .append('g')
-      //   .attr('class','category_section')
-      //   .on('click', category_key_click);
-
-      // inst_group
-      //   .append('div')
-      //   .attr('class','category_color')
-      //   .style('width','15px')
-      //   .style('height','15px')
-      //   .style('float','left')
-      //   .style('margin-right','5px')
-      //   .style('margin-top','2px')
-      //   .style('background-color',function(){
-      //     var inst_color = params.viz.cat_colors.col[inst_cat];
-      //     return inst_color;
-      //   });
+      inst_group
+        .append('div')
+        .attr('class','category_color')
+        .style('width','15px')
+        .style('height','15px')
+        .style('float','left')
+        .style('margin-right','5px')
+        .style('margin-top','2px')
+        .style('background-color',function(){
+          var inst_color = params.viz.cat_colors.col[inst_cat];
+          return inst_color;
+        });
 
 
-      // inst_group
-      //   .append('p')
-      //   .style('margin-bottom','2px')
-      //   .append('text')
-      //   .text(inst_cat)
-      //   .attr('class','noselect')
-      //   .style('cursor','pointer');
+      inst_group
+        .append('p')
+        .style('margin-bottom','2px')
+        .append('text')
+        .text(inst_cat)
+        .attr('class','noselect')
+        .style('cursor','pointer');
 
     }
 
   }
+
+
+  function category_key_click(){
+    
+    var inst_cat = d3.select(this).select('text').text();
+
+    console.log(inst_cat)
+
+    // // update the category 
+    // if (cgm.params.current_col_cat === inst_cat){
+
+    //   // show all categories 
+    //   cgm.change_category('show_all'); 
+
+    //   // show selection in key 
+    //   d3.selectAll('.category_section')
+    //     .select('.category_color')
+    //     .style('opacity',1);
+
+    //   d3.selectAll('.category_section')
+    //     .select('p')
+    //     .style('opacity',1);
+
+    //   ini_sliders();
+
+    // } else {
+
+    //   // show one category 
+    //   cgm.change_category(inst_cat); 
+
+    //   // show selection in key 
+    //   d3.selectAll('.category_section')
+    //     .select('.category_color')
+    //     .style('opacity',0.35);
+
+    //   d3.selectAll('.category_section')
+    //     .select('p')
+    //     .style('opacity',0.35);
+
+    //   d3.select(this)
+    //     .select('.category_color')
+    //     .style('opacity',1);
+
+    //   d3.select(this)
+    //     .select('p')
+    //     .style('opacity',1);
+
+    //   ini_sliders();
+    // }
+
+    // // update the network after changing the category 
+    // cgm.update_network('default');
+
+  }  
 
 };
