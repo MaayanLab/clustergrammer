ed5bb95779478ccad060033bf558b2d5b69cd9d9
cornhundred <doctorcorn2002@gmail.com>
2016-03-15 16:41:48 -0400

    unstable, brought in script to produce multiple category tsv files

--- load_clustergram.js
+++ load_clustergram.js
@@ -1,5 +1,6 @@
 
-make_clust('mult_view.json')/**/;
+// make_clust('mult_view.json')/**/;
+make_clust('mult_cats.json')/**/;
 // make_clust('large_vect_post_example.json');
 // make_clust('vect_post_example.json');
 // make_clust('enr_clust_example.json');
