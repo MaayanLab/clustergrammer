1a2c715270b3d2e1c2e394c5e7f172b7a333104f
cornhundred <doctorcorn2002@gmail.com>
2016-03-28 15:33:47 -0400

    making category keys in separate function within sidebar

--- src/network/change_network_view.js
+++ src/network/change_network_view.js
@@ -1,15 +1,22 @@
 var filter_using_new_nodes = require('./filter_using_new_nodes');
 
-module.exports = function(params, orig_network_data, requested_view) {
+module.exports = function change_network_view(params, orig_network_data, requested_view) {
 
   var views = orig_network_data.views;
   var filter_type = Object.keys(requested_view);
   var filter_value = requested_view[filter_type];
 
-  var inst_view = _.filter(views, function(d){return d[filter_type]==filter_value;})[0];
+  var inst_view = _.filter(views, function(d){
+    return d[filter_type]==filter_value;
+  });
 
-  // console.log('change network view - find the requested view')
-  // console.log(inst_view)
+  if (inst_view.length==1){
+    inst_view = inst_view[0];
+  } else {
+    console.log('\n\n----- found more than one view')
+    console.log(inst_view)
+    inst_view = inst_view[0];
+  }
 
   var new_network_data;
 
