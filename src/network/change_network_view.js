3064cf53153d81fcc63312b1ae79db45680c460d
cornhundred <doctorcorn2002@gmail.com>
2016-03-29 16:21:32 -0400

    fixed view updating for categorical filtering (specifically for Enrichr for now)

--- src/network/change_network_view.js
+++ src/network/change_network_view.js
@@ -1,4 +1,5 @@
 var filter_using_new_nodes = require('./filter_using_new_nodes');
+var utils = require('../utils');
 
 module.exports = function change_network_view(params, orig_network_data, requested_view) {
 
@@ -6,15 +7,31 @@ module.exports = function change_network_view(params, orig_network_data, request
   var inst_view;
   var inst_value;
   var request_filters = _.keys(requested_view);
+  var found_filter;
 
   // find a view that matches all of the requested view/filter-attributes 
   _.each(request_filters, function(inst_filter){
 
-    inst_value = parseInt(requested_view[inst_filter],10);
+    inst_value =  requested_view[inst_filter];
 
-    views = _.filter(views, function(d){
-      return d[inst_filter] == inst_value;
+    // if the value is a number, then convert it to an integer 
+    if ( /[^a-z_]/i.test( inst_value ) ){
+      inst_value = parseInt(inst_value,10);
+    } 
+
+    // only run filtering if any of the views has the filter 
+    found_filter = false;
+    _.each(views, function(tmp_view){
+      if ( utils.has(tmp_view, inst_filter) ){
+        found_filter = true;
+      }
     });
+
+    if (found_filter){
+      views = _.filter(views, function(d){
+        return d[inst_filter] == inst_value;
+      });
+    }
 
   });
 
