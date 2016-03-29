be521cfc9578735f3aa4921378dd487b5716a1d8
cornhundred <doctorcorn2002@gmail.com>
2016-03-29 12:27:28 -0400

    working on col filtering

--- src/filters/apply_filter_slider.js
+++ src/filters/apply_filter_slider.js
@@ -7,13 +7,19 @@ module.exports = function apply_filter_slider(config, params, filter_type, avail
   // get value
   var inst_index = $( params.root+' .slider_'+filter_type ).slider( "value" );
   var inst_state = available_views[inst_index][filter_type];
-  var requested_view = {};
-  requested_view[filter_type] = inst_state;
 
   reset_other_filter_sliders(params, filter_type, inst_state);
 
   params = get_current_orders(params);
   
+  var requested_view = {};
+  requested_view[filter_type] = inst_state;
+
+  if ( _.has(available_views[0],'enr_score_type') ){
+    console.log('\n---- found enr_score_type --- ')
+    console.log('perform additional filtering')
+  }
+
   params = update_network(config, params, requested_view);
 
   return params;
