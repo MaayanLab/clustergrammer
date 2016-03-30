cda25dadd3415bcc58f249b4264d4155d3f5f68c
cornhundred <doctorcorn2002@gmail.com>
2016-03-30 16:04:07 -0400

    added title and instruction to the sidebar that is left blank for the user to fill in

--- src/sidebar/index.js
+++ src/sidebar/index.js
@@ -12,6 +12,14 @@ module.exports = function sidebar(config, params) {
 
   var sidebar = d3.select(params.root+' .sidebar_wrapper');
 
+  sidebar
+    .append('div')
+    .classed('title_section',true);
+
+  sidebar
+    .append('div')
+    .classed('about_section',true)
+
   set_up_reorder(params, sidebar);
 
   set_up_search(sidebar, params);
