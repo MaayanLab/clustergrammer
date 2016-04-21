1d329857ef7a1c1cf630f10cd5467412a4e84164
cornhundred <doctorcorn2002@gmail.com>
2016-04-21 15:54:28 -0400

    removed console log

--- src/dendrogram/reset_cat_opacity.js
+++ src/dendrogram/reset_cat_opacity.js
@@ -1,6 +1,5 @@
 module.exports = function reset_cat_opacity(params){
 
-  console.log('reset_cat_opacity')
   _.each(['row','col'], function(inst_rc){
     d3.selectAll(params.root+' .'+inst_rc+'_cat_group')
       .selectAll('rect')
