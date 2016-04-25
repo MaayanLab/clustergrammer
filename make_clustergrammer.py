0b043e95fbe5b0058bcc796cc1d1d92bbcf323fb
cornhundred <doctorcorn2002@gmail.com>
2016-04-25 13:37:26 -0400

    working on tracking original non-normalized data in mat_orig

--- make_clustergrammer.py
+++ make_clustergrammer.py
@@ -19,19 +19,26 @@ net.load_file('txt/rc_two_cats.txt')
 # net.load_file('txt/number_names.txt')
 
 
-views = ['N_row_sum', 'N_row_var']
-# views = ['N_row_sum']
 
 # # filtering rows and cols by sum 
 # net.filter_sum('row', threshold=20)
 # net.filter_sum('col', threshold=30)
 
-# net.filter_N_top('row', 500, 'sum')
-
+# # quantile normalize to normalize cell lines
 # net.normalize(axis='col', norm_type='qn')
+
+# # only keep most differentially expressed genes
+# net.filter_N_top('row', 700, 'sum')
+
+# # take zscore
+# net.normalize(axis='row', norm_type='zscore')
+
 
 net.swap_nan_for_zero()
   
+# views = ['N_row_sum', 'N_row_var']
+views = []
+
 net.make_clust(dist_type='cos',views=views , dendro=True,
                sim_mat=True, filter_sim=0.1)
 
