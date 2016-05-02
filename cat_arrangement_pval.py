5a395a0b520047758d3a45ad4c103138dd93bcef
cornhundred <doctorcorn2002@gmail.com>
2016-05-02 16:36:09 -0400

    working on calc pvalue for category distribution

--- cat_arrangement_pval.py
+++ cat_arrangement_pval.py
@@ -5,38 +5,55 @@ def main():
 
   net.load_file('txt/rc_two_cats.txt')
 
-  # make_distance_matrix(net)
+  tmp_size = 1000
 
-  randomly_sample_rows(net)
+  inst_dm = make_distance_matrix(net, tmp_size)
 
-def randomly_sample_rows(net):
+  randomly_sample_rows(net, inst_dm, tmp_size)
+
+def randomly_sample_rows(net, inst_dm, tmp_size):
   import numpy as np
+  import pandas as pd 
   inst_rows = net.dat['nodes']['row']
 
-  # print(inst_rows)
+  inst_rows = np.asarray(inst_rows[:tmp_size])
 
-  inst_rows = np.asarray(inst_rows[:10])
+  df = pd.DataFrame(data=inst_dm)
+
+  inst_rows = df.index.tolist()
+
+  mean_dist = []
+
+  num_null = 20000
+  num_points = 50
 
   np.random.seed(100)
-  for i in range(10):
-    tmp = np.random.choice(inst_rows, 3)
-    print(tmp)
+  for i in range(num_null):
+    tmp = np.random.choice(inst_rows, num_points, replace=False)
 
-def make_distance_matrix(net):
+    # print( df[tmp].ix[tmp] )
+    # print('mean: \n------------')
+    mean_dist.append( np.mean(df[tmp].ix[tmp].values) )
+    # print('------------\n')
+
+  mean_dist = np.asarray(mean_dist)
+
+  s1 = pd.Series(mean_dist)
+  hist = np.histogram(s1)
+
+  print(hist[0]/np.float(num_null))
+  print(hist[1])
+
+  # count, division = np.histogramm(mea?n_dist)
+
+  # print(count)
+
+
+def make_distance_matrix(net, tmp_size):
   import numpy as np 
-  inst_cols = net.dat['nodes']['col']
 
-  # mat = np.asarray(range(len(inst_cols)))
-
-  mat = net.dat['mat']
-
-  # mat = mat[0:10, 0:1]
-  # print(mat)
-  # print('\n')
-  # print(mat.shape)
-
-  mat = np.zeros([10,1])
-  mat[:,0] = range(10)
+  mat = np.zeros([tmp_size,1])
+  mat[:,0] = range(tmp_size)
 
   inst_rc = 'row'
   dist_type = 'euclidean'
@@ -52,11 +69,7 @@ def make_distance_matrix(net):
 
   inst_dm = squareform(inst_dm)
 
-  print(inst_dm.shape)
-  print(inst_dm)
   tmp_mean = np.mean(inst_dm)
-  print(inst_dm.shape)
-  print(tmp_mean)
-
+  return inst_dm
 
 main()
