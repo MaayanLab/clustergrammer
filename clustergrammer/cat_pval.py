ce1d747f3f2483abd74a3fdb784b01d69b625845
cornhundred <doctorcorn2002@gmail.com>
2016-05-02 18:14:40 -0400

    calculating histograms of distances, will use this to estimate the pvalue of category grouping

--- clustergrammer/cat_pval.py
+++ clustergrammer/cat_pval.py
@@ -16,16 +16,21 @@ def main(net):
   # reorder based on clustered order 
   inst_cols = [ inst_cols[i] for i in inst_index]
 
+  # make distance matrix dataframe 
   dm = dist_matrix_lattice(inst_cols)
 
   tmp_dict = net.dat['node_info']['col']['dict_cat_0']
 
-  print(dm)
-
   for inst_cat in tmp_dict:
-    print(tmp_dict[inst_cat])
+    subset = tmp_dict[inst_cat]
+    print(subset)
+    inst_mean = calc_mean_dist_subset(dm, subset)
+    print(inst_mean)
 
-    # calc_mean_dist_subset(dm, )
+    hist = calc_hist_distances(dm, subset, inst_cols)
+    print(hist['prob'])
+    print(hist['bins'])
+    print('\n\n')
 
 def dist_matrix_lattice(names):  
   from scipy.spatial.distance import pdist, squareform
@@ -45,6 +50,30 @@ def dist_matrix_lattice(names):
   return df
 
 
-def calc_mean_dist_subset(df, subset):
-  pass
+def calc_mean_dist_subset(dm, subset):
+  return np.mean(dm[subset].ix[subset].values)
   
+def calc_hist_distances(dm, subset, inst_cols):
+  np.random.seed(100)
+
+  num_null = 1000
+  num_points = len(subset)
+
+  mean_dist = []
+  for i in range(num_null):
+    tmp = np.random.choice(inst_cols, num_points, replace=False)
+    mean_dist.append( np.mean(dm[tmp].ix[tmp].values)  )
+
+  tmp_dist = sorted(deepcopy(mean_dist))
+  print('lowest distances')
+  print(tmp_dist[0:4])
+
+  mean_dist = np.asarray(mean_dist)
+  s1 = pd.Series(mean_dist)
+  hist = np.histogram(s1, bins=20)
+
+  H = {}
+  H['prob'] = hist[0]/np.float(num_null)
+  H['bins'] = hist[1]
+
+  return H
