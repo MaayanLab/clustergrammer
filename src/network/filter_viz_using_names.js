7ea23bb1022c193aa5cefc876a6abd06c3356bda
cornhundred <doctorcorn2002@gmail.com>
2016-07-01 11:22:45 -0400

    external name filtering is almost working for rows and cols

--- src/network/filter_viz_using_names.js
+++ src/network/filter_viz_using_names.js
@@ -3,15 +3,33 @@ var update_viz_with_network = require('../update/update_viz_with_network');
 
 module.exports = function filter_viz_using_names(names){
 
-  var row_nodes = this.params.network_data.row_nodes;
+  // names is an object with row and column names that will be used to filter
+  // the matrix
 
-  var found_nodes = $.grep(row_nodes, function(d){
-    return $.inArray(d.name, names) > -1 ;
+  var params = this.params;
+  var new_nodes = {};
+  var found_nodes;
+
+  _.each(['row', 'col'], function(inst_rc){
+
+    var orig_nodes = params.network_data[inst_rc+'_nodes'];
+
+    if (_.has(names, inst_rc)){
+
+      var inst_names = names[inst_rc];
+      found_nodes = $.grep(orig_nodes, function(d){
+        return $.inArray(d.name, inst_names) > -1 ;
+      });
+
+    } else {
+      found_nodes = orig_nodes;
+    }
+
+    new_nodes[inst_rc+'_nodes'] = found_nodes;
+
   });
 
-  var new_nodes = {};
-  new_nodes.row_nodes = found_nodes;
-  new_nodes.col_nodes = this.params.network_data.col_nodes;
+  // new_nodes.col_nodes = params.network_data.col_nodes;
 
   var new_network_data = filter_network_using_new_nodes(this.config, new_nodes);
   update_viz_with_network(this, new_network_data);
