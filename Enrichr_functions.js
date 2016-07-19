ea4b137d4ee704692e694967c9dacf392ac44764
cornhundred <doctorcorn2002@gmail.com>
2016-07-19 17:23:10 -0400

    working on callback passing that will ultimately be used to update the visualization

--- Enrichr_functions.js
+++ Enrichr_functions.js
@@ -1,83 +1,83 @@
 
-function get_enr_with_list(gene_list, library){
-  enr_obj.post_list(gene_list, function(){
-    console.log('\nrunning after list has been posted\n')
-    enr_obj.get_enr(library);
-  });
+function Enrichr_request(){
+  function get_enr_with_list(gene_list, library){
+    enr_obj.post_list(gene_list, function(){
+      console.log('\nrunning after list has been posted\n')
+      enr_obj.get_enr(library);
+    });
 
-}
-
-function post_list(gene_list, callback_function){
-
-  var gene_list_string = gene_list.join('\n');
-
-  var form = new FormData();
-  var response;
-
-  form.append("list", gene_list_string);
-
-  form.append("description", "clustergrammer");
-
-  var settings = {
-   "async": true,
-   "crossDomain": true,
-   "url": "http://amp.pharm.mssm.edu/Enrichr/addList",
-   "method": "POST",
-   "processData": false,
-   "contentType": false,
-   "mimeType": "multipart/form-data",
-   "data": form
   }
 
-  if (typeof callback_function === 'undefined'){
-    callback_function = confirm_save;
-  }
+  function post_list(gene_list, callback_function){
 
-  $.ajax(settings)
-   .done(function(response){
-    response = JSON.parse(response);
-    enr_obj.user_list_id = response.userListId;
-    callback_function(response);
-   });
-}
+    var gene_list_string = gene_list.join('\n');
 
-function confirm_save(response){
-  console.log('saved user_list_id '+String(enr_obj.user_list_id));
-}
-
-function get_enr(library){
-
-  if (enr_obj.user_list_id !== null){
     var form = new FormData();
+    var response;
 
-    var base_url = 'http://amp.pharm.mssm.edu/Enrichr/enrich?';
-    var library_string = 'backgroundType=' + String(library);
-    var list_id_string = 'userListId=' + String(enr_obj.user_list_id);
+    form.append("list", gene_list_string);
 
-    var full_url = base_url + library_string + '&' + list_id_string;
+    form.append("description", "clustergrammer");
 
-    // get request
     var settings = {
      "async": true,
      "crossDomain": true,
-     "url": full_url,
-     "method": "GET",
+     "url": "http://amp.pharm.mssm.edu/Enrichr/addList",
+     "method": "POST",
      "processData": false,
      "contentType": false,
      "mimeType": "multipart/form-data",
      "data": form
     }
 
-    $.ajax(settings).done(function (response) {
-      response = JSON.parse(response);
-      enr_obj.enr_data = response;
-    });
-  } else {
-    console.log('no user_list_id defined')
-  }
-}
+    if (typeof callback_function === 'undefined'){
+      callback_function = confirm_save;
+    }
 
-function Enrichr_request(){
+    $.ajax(settings)
+     .done(function(response){
+      response = JSON.parse(response);
+      enr_obj.user_list_id = response.userListId;
+      callback_function(response);
+     });
+  }
+
+  function confirm_save(response){
+    console.log('saved user_list_id '+String(enr_obj.user_list_id));
+  }
+
+  function get_enr(library){
+
+    if (enr_obj.user_list_id !== null){
+      var form = new FormData();
+
+      var base_url = 'http://amp.pharm.mssm.edu/Enrichr/enrich?';
+      var library_string = 'backgroundType=' + String(library);
+      var list_id_string = 'userListId=' + String(enr_obj.user_list_id);
+
+      var full_url = base_url + library_string + '&' + list_id_string;
+
+      // get request
+      var settings = {
+       "async": true,
+       "crossDomain": true,
+       "url": full_url,
+       "method": "GET",
+       "processData": false,
+       "contentType": false,
+       "mimeType": "multipart/form-data",
+       "data": form
+      }
+
+      $.ajax(settings).done(function (response) {
+        response = JSON.parse(response);
+        enr_obj.enr_data = response;
+      });
+    } else {
+      console.log('no user_list_id defined')
+    }
+  }
+
 
   // example of how to check gene list
   // http://amp.pharm.mssm.edu/Enrichr/view?userListId=1284420
