7119517e81134474b83dcbe317a79e8b000044ed
cornhundred <doctorcorn2002@gmail.com>
2016-07-26 14:19:38 -0400

    unstable, improved inst_nodes category behavior. working on upating network category behavior

--- src/update/update_cats.js
+++ src/update/update_cats.js
@@ -4,33 +4,34 @@ var resize_viz = require('../reset_size/resize_viz');
 
 module.exports = function update_cats(cgm, cat_data){
 
-    // console.log('---------\nupdate_Cats\n-------------')
-    // console.log(cat_data)
+  var cat_type_num = 0;
+  var inst_index = 0;
+  var inst_cat_title;
+  var inst_cats;
+  var inst_members;
+  var inst_name;
+  var inst_category;
+  var inst_cat_name;
+  var inst_full_cat;
+  var inst_cat_num;
 
+  modify_row_node_cats(cgm.params.network_data.row_nodes);
+  modify_row_node_cats(cgm.params.inst_nodes.row_nodes);
 
-    var cat_type_num = 0;
-    var inst_index = 0;
-    var inst_cat_title;
-    var inst_cats;
-    var inst_members;
-    var inst_name;
-    var inst_category;
-    var inst_cat_name;
-    var inst_full_cat;
-    var inst_cat_num;
+  // recalculate the visualization parameters using the updated network_data
+  cgm.params = calc_viz_params(cgm.params, false);
 
+  make_row_cat(cgm.params, true);
+  resize_viz(cgm);
+
+  function modify_row_node_cats(inst_nodes){
     // loop through row nodes
     //////////////////////////
-    _.each(cgm.params.network_data.row_nodes, function(inst_node){
+    _.each(inst_nodes, function(inst_node){
 
       inst_name = inst_node.name;
-
-      // console.log(inst_name)
-      // console.log('***************')
-
-
       cat_type_num = 0
-      // loop through each category-type
+
       _.each(cat_data, function(inst_cat_data){
 
 
@@ -65,40 +66,11 @@ module.exports = function update_cats(cgm, cat_data){
         inst_node['cat-'+String(cat_type_num)] = inst_full_cat
         inst_node['cat_'+String(cat_type_num)+'_index'] = inst_index
 
-        // console.log(inst_full_cat)
-        // console.log(cat_type_num)
-
         cat_type_num = cat_type_num + 1
       });
 
-      // console.log('\n\n')
-
-      // var is_interesting = false;
-      // if (inst_index < 10){
-      //   is_interesting = true;
-      // }
-
-      // inst_node['cat_0_index'] = inst_index
-      // inst_node['cat-0'] = 'Very Interesting: '+String(is_interesting);
-
-      // inst_node['cat_1_index'] = inst_index
-      // inst_node['cat-1'] = 'Very Interesting: '+String(is_interesting);
-
-      // inst_node['cat_2_index'] = inst_index
-      // inst_node['cat-2'] = 'Very Interesting: '+String(is_interesting);
-
-      // inst_index = inst_index + 1;
-
     });
+  }
 
-    // console.log('update_cats')
-
-    // recalculate the visualization parameters using the updated network_data
-    cgm.params = calc_viz_params(cgm.params, false);
-
-    // console.log(cgm.params.viz.cat_colors.row)
-
-    make_row_cat(cgm.params, true);
-    resize_viz(cgm);
 
 };
