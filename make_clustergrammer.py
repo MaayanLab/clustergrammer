d10da5a626f6eeb55fbb99b291295d86dc11bcee
cornhundred <doctorcorn2002@gmail.com>
2016-08-26 16:35:21 -0400

    cleaning make_clustergrammer.py

--- make_clustergrammer.py
+++ make_clustergrammer.py
@@ -1,13 +1,12 @@
-import time
-start_time = time.time()
+'''
+  You can install clustergrammer python module using pip:
+  pip install clustergrammer
+
+  or get the source code from
+  https://github.com/MaayanLab/clustergrammer-py
+'''
 
 from clustergrammer import Network
-
-# install clustergrammer using pip, see below
-# pip install clustergrammer
-#
-# or install from source code available from
-# https://github.com/MaayanLab/clustergrammer-py
 
 net = Network()
 
@@ -17,45 +16,20 @@ net.load_file('txt/rc_two_cats.txt')
 # net.load_file('txt/ccle_example.txt')
 # net.load_file('txt/tuple_cats.txt')
 
-# # upload vector format
-# ########################
-# vect_format = net.load_json_to_dict('json/fake_vect_post.json')
-# net.load_vect_post_to_net(vect_format)
-
-# link = net.Iframe_web_app('txt/rc_two_cats.txt', width=1000, height=800)
-# link = net.Iframe_web_app( width=1000, height=800)
-
 # possible filtering and normalization
 ##########################################
 # net.filter_sum('row', threshold=20)
-# net.filter_sum('col', threshold=30)
-
-# net.normalize(axis='row', norm_type='qn')
 # net.normalize(axis='col', norm_type='zscore', keep_orig=True)
-
 # net.filter_N_top('row', 250, rank_type='sum')
-# net.filter_N_top('col', 3, rank_type='var')
-
-# net.filter_threShold('col', threshold=2, num_occur=3
 # net.filter_threshold('row', threshold=3.0, num_occur=4)
-
 # net.swap_nan_for_zero()
-
-# df = net.dat_to_df()
 
 views = ['N_row_sum', 'N_row_var']
 
 net.make_clust(dist_type='cos',views=views , dendro=True,
                sim_mat=True, filter_sim=0.1, calc_cat_pval=False)
-               # run_enrichr=['KEA_2015'])
-               # run_enrichr=['ENCODE_TF_ChIP-seq_2014'])
-               # run_enrichr=['GO_Biological_Process_2015'])
 
+# write jsons for front-end visualizations
 net.write_json_to_file('viz', 'json/mult_view.json', 'no-indent')
 net.write_json_to_file('sim_row', 'json/mult_view_sim_row.json', 'no-indent')
 net.write_json_to_file('sim_col', 'json/mult_view_sim_col.json', 'no-indent')
-
-# net.write_matrix_to_tsv ('txt/export_tmp.txt')
-
-elapsed_time = time.time() - start_time
-print('\n\nelapsed time: '+str(elapsed_time))
