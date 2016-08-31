65b192e8db0bcde24287fbbb4e62707bb0e55e0b
cornhundred <doctorcorn2002@gmail.com>
2016-08-31 11:36:30 -0400

    working on bundling font-awesome into webpack

--- webpack.config.js
+++ webpack.config.js
@@ -27,7 +27,9 @@ module.exports = {
               query: {
                 presets: ['es2015']
               }
-            }
+            },
+           { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=10000&minetype=application/font-woff" },
+           { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "file-loader" }
         ]
     }
 };
