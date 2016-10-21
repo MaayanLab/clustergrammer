532901acd8b48d60e8704ec1195aec8bf1757da5
Nick Fernandez <nickfloresfernandez@gmail.com>
2016-10-21 16:54:43 -0400

    made python3 specific fix, forcing values to be float

--- make_clustergrammer.py
+++ make_clustergrammer.py
@@ -12,8 +12,8 @@ net = Network()
 
 # load matrix tsv file
 net.load_file('txt/rc_two_cats.txt')
-# net.load_file('txt/mnist.txt')
 # net.load_file('txt/rc_val_cats.txt')
+# net.load_file('txt/mnist.txt')
 # net.load_file('txt/tuple_cats.txt')
 # net.load_file('txt/example_tsv.txt')
 
