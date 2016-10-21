39fa58600563612168306d58712d9119f2ba5227
Nick Fernandez <nickfloresfernandez@gmail.com>
2016-10-21 11:03:58 -0400

    improved cat-value hover

--- clustergrammer.js
+++ clustergrammer.js
@@ -6730,8 +6730,10 @@ var Clustergrammer =
 	  var cat_title = get_cat_title(params.viz, inst_cat, inst_rc);
 	  var cat_name = inst_data[inst_cat];
 
-	  if (cat_name.indexOf(': ') >= 0) {
-	    cat_name = cat_name.split(': ')[1];
+	  if (typeof cat_name === 'string') {
+	    if (cat_name.indexOf(': ') >= 0) {
+	      cat_name = cat_name.split(': ')[1];
+	    }
 	  }
 
 	  var cat_string = cat_title + ': ' + cat_name;
@@ -6753,6 +6755,8 @@ var Clustergrammer =
 
 	  function highlight_categories() {
 
+	    var run_highlighting = false;
+
 	    if (d3.select(inst_selection).classed('hovering')) {
 
 	      var node_types = [inst_rc];
@@ -6763,8 +6767,14 @@ var Clustergrammer =
 
 	      _.each(node_types, function (tmp_rc) {
 
-	        if (cat_name.indexOf('Not ') < 0 && cat_name != 'false') {
+	        // only highlight string categories that are not 'false' categories
+	        if (cat_name === 'string') {
+	          if (cat_name.indexOf('Not ') < 0 && cat_name != 'false') {
+	            run_highlighting = true;
+	          }
+	        }
 
+	        if (run_highlighting) {
 	          d3.selectAll(params.root + ' .' + tmp_rc + '_cat_group').selectAll('rect').style('opacity', function (d) {
 
 	            var inst_opacity = d3.select(this).style('opacity');
--- src/dendrogram/cat_tooltip_text.js
+++ src/dendrogram/cat_tooltip_text.js
@@ -8,8 +8,10 @@ module.exports = function cat_tooltip_text(params, inst_data, inst_selection, in
   var cat_title = get_cat_title(params.viz, inst_cat, inst_rc);
   var cat_name = inst_data[inst_cat];
 
-  if (cat_name.indexOf(': ') >=0){
-    cat_name = cat_name.split(': ')[1];
+  if (typeof cat_name === 'string'){
+    if (cat_name.indexOf(': ') >=0){
+      cat_name = cat_name.split(': ')[1];
+    }
   }
 
   var cat_string = cat_title + ': '+ cat_name;
@@ -34,6 +36,8 @@ module.exports = function cat_tooltip_text(params, inst_data, inst_selection, in
 
   function highlight_categories(){
 
+    var run_highlighting = false;
+
     if (d3.select(inst_selection).classed('hovering')){
 
       var node_types = [inst_rc];
@@ -44,8 +48,14 @@ module.exports = function cat_tooltip_text(params, inst_data, inst_selection, in
 
       _.each(node_types, function(tmp_rc){
 
-        if (cat_name.indexOf('Not ') < 0 && cat_name != 'false'){
+        // only highlight string categories that are not 'false' categories
+        if (cat_name === 'string' ){
+          if (cat_name.indexOf('Not ') < 0 && cat_name != 'false'){
+            run_highlighting = true;
+          }
+        }
 
+        if (run_highlighting){
           d3.selectAll(params.root+' .'+tmp_rc+'_cat_group')
             .selectAll('rect')
             .style('opacity', function(d){
@@ -74,6 +84,7 @@ module.exports = function cat_tooltip_text(params, inst_data, inst_selection, in
 
             });
         }
+
 
       });
 
