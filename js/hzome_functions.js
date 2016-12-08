47d685c6b6824f07608088fd7f3287da0e835ab6
Nick Fernandez <nickfloresfernandez@gmail.com>
2016-12-08 17:39:26 -0500

    setting up callback function to check whether any genes were found in list of names

--- js/hzome_functions.js
+++ js/hzome_functions.js
@@ -1,8 +1,10 @@
 
 function ini_hzome(){
 
-  // save gene data
+  // save gene data to global variable
   gene_data = {};
+
+  were_genes_found = false;
 
   function get_mouseover(gene_symbol){
 
@@ -14,7 +16,13 @@ function ini_hzome(){
 
   }
 
-  function get_request(gene_symbol){
+  function get_request(gene_symbol, optional_callback){
+
+    if (typeof optional_callback === 'function'){
+      optional_callback(gene_symbol);
+    } else {
+      console.log('no callback function given')
+    }
 
     var base_url = 'https://amp.pharm.mssm.edu/Harmonizome/api/1.0/gene/';
     var url = base_url + gene_symbol;
@@ -23,10 +31,16 @@ function ini_hzome(){
 
       data = JSON.parse(data);
 
+      // console.log(gene_symbol + ': ' + data.name + '\n----------------\n');
+
       // save data for repeated use
       gene_data[gene_symbol] = {}
       gene_data[gene_symbol].name = data.name;
       gene_data[gene_symbol].description = data.description;
+
+      if (data.name != undefined){
+        were_genes_found = true;
+      }
 
       set_tooltip(data, gene_symbol);
 
@@ -73,5 +87,30 @@ function ini_hzome(){
 
 }
 
+function check_for_genes(){
+
+  var all_names = ['EGFR', 'not-a-gene'];
+
+  // check each gene using Harmonizome
+  _.each(all_names, function(inst_name){
+
+    // console.log(inst_name)
+    hzome.get_request(inst_name, check_gene_name);
+
+  });
+
+  console.log(were_genes_found)
+
+}
+
+function check_gene_name(inst_name){
+
+  console.log('\n**************')
+  console.log('checking ' + inst_name)
+  console.log('**************\n')
+
+
+
+}
 
 
