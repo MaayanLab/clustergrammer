38117dab04cec8a4ae95b4fa16a1bbc0f500f68f
Nick Fernandez <nickfloresfernandez@gmail.com>
2016-12-13 14:07:47 -0500

    making simple matrix string with no categories

--- clustergrammer.js
+++ clustergrammer.js
@@ -12587,28 +12587,44 @@ var Clustergrammer =
 	    order_indexes[inst_rc] = inst_cgm.params.matrix.orders[inst_order_name + '_' + tmp_rc];
 	  });
 
+	  var matrix_string = '\t';
+
 	  var row_data = inst_cgm.params.matrix.matrix[0].row_data;
+
 	  console.log('cols');
 	  _.each(order_indexes['col'], function (inst_index) {
 
 	    // column names
 	    console.log(row_data[inst_index].col_name);
+
+	    matrix_string = matrix_string + row_data[inst_index].col_name + '\t';
 	  });
 
+	  console.log('\n\n\n');
+
+	  matrix_string = matrix_string + '\n';
+
+	  console.log('rows');
 	  _.each(order_indexes['row'], function (inst_index) {
 
 	    // row names
-	    console.log('rows');
-	    console.log('\n\n\n');
-	    console.log(inst_cgm.params.matrix.matrix[inst_index].name);
-
 	    row_data = inst_cgm.params.matrix.matrix[inst_index].row_data;
 
 	    console.log('\n');
+	    console.log(inst_cgm.params.matrix.matrix[inst_index].name);
+
+	    matrix_string = matrix_string + inst_cgm.params.matrix.matrix[inst_index].name + '\t';
+
 	    _.each(order_indexes['col'], function (col_index) {
 	      console.log(String(row_data[col_index].col_name) + ': ' + String(row_data[col_index].value));
+
+	      matrix_string = matrix_string + String(row_data[col_index].value) + '\t';
 	    });
+
+	    matrix_string = matrix_string + '\n';
 	  });
+
+	  console.log(matrix_string);
 		};
 
 /***/ }
