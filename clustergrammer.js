3ac40cb2e9016874d941054269a8cf64a86e34c2
Nick Fernandez <nickfloresfernandez@gmail.com>
2017-01-04 16:15:43 -0500

    improved zooming uncropping behavior for crop buttons;

--- clustergrammer.js
+++ clustergrammer.js
@@ -3922,6 +3922,8 @@ var Clustergrammer =
 	      // use class as 'global' variable
 	      d3.select(cgm.params.root + ' .' + inst_rc + '_dendro_icons_group').attr('transform', 'translate(0,0), scale(1,1)').classed('ran_filter', true);
 
+	      d3.select(cgm.params.root + ' .' + other_rc + '_dendro_icons_group').attr('transform', 'translate(0,0), scale(1,1)');
+
 	      // do not display dendrogram slider if filtering has been run
 	      d3.select(cgm.params.root + ' .' + inst_rc + '_slider_group').style('display', 'none');
 
--- src/dendrogram/make_dendro_crop_buttons.js
+++ src/dendrogram/make_dendro_crop_buttons.js
@@ -267,6 +267,9 @@ module.exports = function make_dendro_crop_buttons(cgm, inst_rc){
           .attr('transform', 'translate(0,0), scale(1,1)')
           .classed('ran_filter', true);
 
+        d3.select(cgm.params.root+' .'+other_rc+'_dendro_icons_group')
+          .attr('transform', 'translate(0,0), scale(1,1)')
+
         // do not display dendrogram slider if filtering has been run
         d3.select(cgm.params.root+' .'+inst_rc+'_slider_group')
           .style('display', 'none');
