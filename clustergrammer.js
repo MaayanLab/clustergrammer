92d66c3fcde0491e1ba79fafd338d527c79836bf
Nick Fernandez <nickfloresfernandez@gmail.com>
2017-01-05 18:07:55 -0500

    improving border_width.y use

--- clustergrammer.js
+++ clustergrammer.js
@@ -13217,7 +13217,7 @@ var Clustergrammer =
 
 	module.exports = function fine_position_tile(params, d) {
 	  var x_pos = params.viz.x_scale(d.pos_x) + 0.5 * params.viz.border_width.x;
-	  var y_pos = 0.5 * params.viz.border_width.y / params.viz.zoom_switch;
+	  var y_pos = 0.5 * params.viz.border_width.y;
 	  return 'translate(' + x_pos + ',' + y_pos + ')';
 	};
 
