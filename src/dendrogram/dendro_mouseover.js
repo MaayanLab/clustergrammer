70e766286fac24c5b5c9ed9924c36aeb0abe4aef
cornhundred <nickfloresfernandez@gmail.com>
2017-01-06 14:29:16 -0500

    working on passing group during mouseover

--- src/dendrogram/dendro_mouseover.js
+++ src/dendrogram/dendro_mouseover.js
@@ -1,4 +1,9 @@
-module.exports = function dendro_mouseover(cgm, inst_selection){
+module.exports = function dendro_mouseover(cgm, inst_selection, inst_data){
+
+  console.log(inst_data.all_names)
+
+  // find cats: cat-strings
+  //////////////////////////
 
   // run instantly on mouseover
   d3.select(inst_selection)
