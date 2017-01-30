54c74adfe2dcc3fe5735d0e6975bf557e4cd1806
Nick Fernandez <nickfloresfernandez@gmail.com>
2017-01-30 16:57:20 -0500

    unstable, working on defining ds visible rows

--- src/zoom/find_viz_nodes.js
+++ src/zoom/find_viz_nodes.js
@@ -1,7 +1,6 @@
 module.exports = function find_viz_nodes(params, viz_area){
 
-  var inst_rows = [];
-  var inst_cols = [];
+  var should_be_rows = [];
   var curr_rows = [];
 
   // find rows that should be visible
@@ -12,7 +11,7 @@ module.exports = function find_viz_nodes(params, viz_area){
     y_trans = params.viz.y_scale(i);
 
     if (y_trans < viz_area.max_y && y_trans > viz_area.min_y){
-      inst_rows.push(params.network_data.row_nodes_names[i])
+      should_be_rows.push(params.network_data.row_nodes_names[i])
     }
 
   }
@@ -23,24 +22,8 @@ module.exports = function find_viz_nodes(params, viz_area){
       curr_rows.push(d.name);
     });
 
-  // find visible cols
-  d3.selectAll(params.root+' .col_label_text')
-    .each(function(d){
-      var inst_trans = d3.select(this)
-        .attr('transform');
-
-      var x_trans = Number(inst_trans.split('(')[1].split(',')[0].split(')')[0]);
-
-      if (x_trans < viz_area.max_x && x_trans > viz_area.min_x){
-        inst_cols.push(d.name);
-      }
-
-    });
-
   // nodes that should be visible
-  params.viz.viz_nodes.row = inst_rows;
-  params.viz.viz_nodes.col = inst_cols;
-
+  params.viz.viz_nodes.row = should_be_rows;
   // nodes that are visible
   params.viz.viz_nodes.curr_row = curr_rows;
 
