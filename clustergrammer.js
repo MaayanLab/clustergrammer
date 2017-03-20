7e74ac5f3f5817d3909778f4e1bbb77c4d0d4b81
Nick Fernandez <nickfloresfernandez@gmail.com>
2017-03-20 15:56:16 -0400

    cleaning find_viz_rows, working on toggling col labels

--- clustergrammer.js
+++ clustergrammer.js
@@ -5584,21 +5584,16 @@ var Clustergrammer =
 	  var row_names = params.network_data.row_nodes_names;
 	  var row_class = '.row';
 
-	  // need to turn this on
-	  /////////////////////////////
+	  // if downsampling redefine variables
 	  if (ds_level >= 0) {
 	    y_scale = params.viz.ds[ds_level].y_scale;
-
 	    row_names = d3.range(params.matrix.ds_matrix[ds_level].length).map(String);
-
 	    row_class = '.ds' + String(ds_level) + '_row';
 	  }
 
+	  // find rows that should be visible
 	  for (var i = 0; i < row_names.length; i++) {
-
-	    // y_scale (works for downsampled data or non-downsampled data)
 	    y_trans = y_scale(i);
-
 	    if (y_trans < viz_area.max_y && y_trans > viz_area.min_y) {
 	      should_be_rows.push(row_names[i]);
 	    }
--- src/zoom/find_viz_rows.js
+++ src/zoom/find_viz_rows.js
@@ -12,26 +12,19 @@ module.exports = function find_viz_rows(params, viz_area){
   var row_names = params.network_data.row_nodes_names;
   var row_class = '.row';
 
-  // need to turn this on
-  /////////////////////////////
+  // if downsampling redefine variables
   if (ds_level >=0){
     y_scale = params.viz.ds[ds_level].y_scale;
-
     row_names = d3.range(params.matrix.ds_matrix[ds_level].length).map(String);
-
     row_class = '.ds'+String(ds_level)+'_row';
   }
 
-
+  // find rows that should be visible
   for (var i=0; i < row_names.length; i++){
-
-    // y_scale (works for downsampled data or non-downsampled data)
     y_trans = y_scale(i);
-
     if (y_trans < viz_area.max_y && y_trans > viz_area.min_y){
       should_be_rows.push(row_names[i]);
     }
-
   }
 
   // find currently visible rows
