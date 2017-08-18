823e0276becfdbb09e9a0f0a903b282b59228060
Nick Fernandez <nickfloresfernandez@gmail.com>
2017-08-18 12:30:30 -0400

    have simple tree icon working, working on making new views based on reordering and toggling between them using the UI

--- clustergrammer.js
+++ clustergrammer.js
@@ -27608,7 +27608,9 @@ var Clustergrammer =
 
 	  var slider_length = 40;
 
-	  var slider_group = d3.select(cgm.params.root + ' .viz_svg').append('g').classed(inst_rc + '_tree_group', true);
+	  var slider_group = d3.select(cgm.params.root + ' .viz_svg').append('g').classed(inst_rc + '_tree_group', true).on('click', function () {
+	    console.log('clicking tree');
+	  });
 
 	  position_svg_dendro_slider(cgm, inst_rc);
 
@@ -27653,14 +27655,38 @@ var Clustergrammer =
 	  slider_group.append('path').style('fill', 'black').attr('transform', 'translate(' + offset_triangle + ', 0)').attr('d', function () {
 
 	    // up triangle
-	    var start_x = 3.4;
-	    var start_y = 27;
+	    var start_x = 4.3;
+	    var start_y = 23;
 
 	    var mid_x = -5; //left_x + slider_length/10;
 	    var mid_y = branch_height / 2.5;
 
-	    var final_x = 5.05; //left_x + slider_length/5;
-	    var final_y = branch_height / 1.5;
+	    var final_x = 5.8; //left_x + slider_length/5;
+	    var final_y = branch_height / 1.8;
+
+	    var output_string = 'M' + start_x + ',' + start_y + ', L' + mid_x + ', ' + mid_y + ', L' + final_x + ',' + final_y + ' Z';
+
+	    return output_string;
+	  }).style('opacity', 0.35).on('mouseover', function () {
+	    d3.selectAll(cgm.params.root + ' .tree_leaf_circle').style('opacity', high_opacity);
+	  }).on('mouseout', function () {
+	    d3.selectAll(cgm.params.root + ' .tree_leaf_circle').style('opacity', default_opacity);
+	  });
+
+	  // right branch
+	  var branch_height = 30;
+	  var right_branch_offset = 15;
+	  slider_group.append('path').style('fill', 'black').attr('transform', 'translate(' + offset_triangle + ', 0)').attr('d', function () {
+
+	    // up triangle
+	    var start_x = 15.7;
+	    var start_y = 23;
+
+	    var mid_x = 25; //left_x + slider_length/10;
+	    var mid_y = branch_height / 2.5;
+
+	    var final_x = 14.2; //left_x + slider_length/5;
+	    var final_y = branch_height / 1.8;
 
 	    var output_string = 'M' + start_x + ',' + start_y + ', L' + mid_x + ', ' + mid_y + ', L' + final_x + ',' + final_y + ' Z';
 
@@ -27675,7 +27701,7 @@ var Clustergrammer =
 	  var high_opacity = 0.6;
 	  var circle_radius = 15;
 	  var small_leaf_offset = 13;
-	  var small_leaf_radius = 9;
+	  var small_leaf_radius = 9.5;
 	  slider_group.selectAll().data([[-3, small_leaf_offset, small_leaf_radius], [tree_width / 2, 0, 17], [23, small_leaf_offset, small_leaf_radius]]).enter().append('circle').classed('tree_leaf_circle', true).attr('r', function (d) {
 	    return d[2];
 	  }).attr('transform', function (d) {
