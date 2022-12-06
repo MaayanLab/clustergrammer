9165a1aed8992fa8638a08452176f0965b3b60f0
Pavel Sidorenko <fat.cat.1703@gmail.com>
2022-12-06 17:20:17 +0100

    fix imports

--- src/main.js
+++ src/main.js
@@ -1,38 +1,40 @@
-var make_config = require('./make_config');
-var make_params = require('./params/make_params');
-var make_viz = require('./make_viz');
-var resize_viz = require('./reset_size/resize_viz');
-var play_demo = require('./demo/play_demo');
-var ini_demo = require('./demo/ini_demo');
-var filter_viz_using_nodes = require('./network/filter_viz_using_nodes');
-var filter_viz_using_names = require('./network/filter_viz_using_names');
-var update_cats = require('./update/update_cats');
-var reset_cats = require('./update/reset_cats');
-var two_translate_zoom = require('./zoom/two_translate_zoom');
-var update_view = require('./update/update_view');
-var save_matrix = require('./matrix/save_matrix');
-var brush_crop_matrix = require('./matrix/brush_crop_matrix');
-var run_zoom = require('./zoom/run_zoom');
-var d3_tip_custom = require('./tooltip/d3_tip_custom');
-var all_reorder = require('./reorder/all_reorder');
-var make_matrix_string = require('./matrix/make_matrix_string');
+var make_config = require("./make_config");
+var make_params = require("./params/make_params");
+var make_viz = require("./make_viz");
+var resize_viz = require("./reset_size/resize_viz");
+var play_demo = require("./demo/play_demo");
+var ini_demo = require("./demo/ini_demo");
+var filter_viz_using_nodes = require("./network/filter_viz_using_nodes");
+var filter_viz_using_names = require("./network/filter_viz_using_names");
+var update_cats = require("./update/update_cats");
+var reset_cats = require("./update/reset_cats");
+var two_translate_zoom = require("./zoom/two_translate_zoom");
+var update_view = require("./update/update_view");
+var save_matrix = require("./matrix/save_matrix");
+var brush_crop_matrix = require("./matrix/brush_crop_matrix");
+var run_zoom = require("./zoom/run_zoom");
+var d3_tip_custom = require("./tooltip/d3_tip_custom");
+var all_reorder = require("./reorder/all_reorder");
+var make_matrix_string = require("./matrix/make_matrix_string");
+var d3 = require("d3");
+var _ = require("underscore");
+var jQuery = require("jquery");
 
 // moved d3.slider to src
-d3.slider = require('./d3.slider');
+d3.slider = require("./d3.slider");
 
 /* eslint-disable */
 
-var awesomplete = require('awesomplete');
+var awesomplete = require("awesomplete");
 // getting css from src
-require('!style!css!./d3.slider/d3.slider.css');
-require('!style!css!awesomplete/awesomplete.css');
+require("!style!css!./d3.slider/d3.slider.css");
+require("!style!css!awesomplete/awesomplete.css");
 
 /* clustergrammer v1.19.5
  * Nicolas Fernandez, Ma'ayan Lab, Icahn School of Medicine at Mount Sinai
  * (c) 2017
  */
 function Clustergrammer(args) {
-
   /* Main program
    * ----------------------------------------------------------------------- */
   // consume and validate user input
@@ -50,16 +52,20 @@ function Clustergrammer(args) {
   cgm.config = config;
 
   // set up zoom
-  cgm.params.zoom_behavior = d3.behavior.zoom()
+  cgm.params.zoom_behavior = d3.behavior
+    .zoom()
     .scaleExtent([1, cgm.params.viz.square_zoom * cgm.params.viz.zoom_ratio.x])
-    .on('zoom', function(){
+    .on("zoom", function () {
       run_zoom(cgm);
     });
 
-  cgm.params.zoom_behavior.translate([cgm.params.viz.clust.margin.left, cgm.params.viz.clust.margin.top]);
+  cgm.params.zoom_behavior.translate([
+    cgm.params.viz.clust.margin.left,
+    cgm.params.viz.clust.margin.top,
+  ]);
 
   if (cgm.params.use_sidebar) {
-    var make_sidebar = require('./sidebar/');
+    var make_sidebar = require("./sidebar/");
     make_sidebar(cgm);
   }
 
@@ -67,52 +73,50 @@ function Clustergrammer(args) {
   make_viz(cgm);
 
   function external_resize() {
-
-    d3.select(cgm.params.viz.viz_svg).style('opacity', 0.5);
+    d3.select(cgm.params.viz.viz_svg).style("opacity", 0.5);
 
     var wait_time = 500;
-    if (this.params.viz.run_trans === true){
+    if (this.params.viz.run_trans === true) {
       wait_time = 2500;
     }
 
     setTimeout(resize_fun, wait_time, this);
   }
 
-  function resize_fun(cgm){
+  function resize_fun(cgm) {
     resize_viz(cgm);
   }
 
-  function run_update_cats(cat_data){
+  function run_update_cats(cat_data) {
     update_cats(this, cat_data);
   }
 
-  function zoom_api(pan_dx, pan_dy, fin_zoom){
+  function zoom_api(pan_dx, pan_dy, fin_zoom) {
     two_translate_zoom(this, pan_dx, pan_dy, fin_zoom);
   }
 
-  function expose_d3_tip_custom(){
+  function expose_d3_tip_custom() {
     // this allows external modules to have access to d3_tip
-    return d3_tip_custom
+    return d3_tip_custom;
   }
 
-  function api_reorder(inst_rc, inst_order){
-    if (inst_order === 'sum'){
-      inst_order = 'rank';
+  function api_reorder(inst_rc, inst_order) {
+    if (inst_order === "sum") {
+      inst_order = "rank";
     }
-    if (inst_order === 'var'){
-      inst_order = 'rankvar';
+    if (inst_order === "var") {
+      inst_order = "rankvar";
     }
-    all_reorder(this, inst_order, inst_rc)
+    all_reorder(this, inst_order, inst_rc);
   }
 
-  function export_matrix_string(){
+  function export_matrix_string() {
     return make_matrix_string(this.params);
   }
 
-  function external_update_view(filter_type, inst_state){
-    update_view(this, filter_type, inst_state)
+  function external_update_view(filter_type, inst_state) {
+    update_view(this, filter_type, inst_state);
   }
-
 
   // add more API endpoints
   cgm.update_view = update_view;
