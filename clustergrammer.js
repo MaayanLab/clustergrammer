5b950f166546fb7303fbe48a29d10edcc71daa60
Pavel Sidorenko <fat.cat.1703@gmail.com>
2022-12-07 17:08:45 +0100

    fix

--- clustergrammer.js
+++ clustergrammer.js
@@ -350,7 +350,8 @@ var Clustergrammer =
 	  var row_has_group = utils.has(config.network_data.row_nodes[0], "group");
 	  var col_has_group = utils.has(config.network_data.col_nodes[0], "group");
 
-	  config.show_dendrogram = row_has_group || col_has_group;
+	  // customization
+	  config.show_dendrogram = args.show_dendrogram ? row_has_group || col_has_group : false;
 
 	  if (utils.has(config.network_data.links[0], "value_orig")) {
 	    config.keep_orig = true;
@@ -2180,7 +2181,9 @@ var Clustergrammer =
 	    dendro_click_callback: null,
 	    new_row_cats: null,
 	    make_modals: true,
-	    show_viz_border: false
+	    show_viz_border: false,
+	    // customization
+	    show_dendrogram: true
 	  };
 
 	  return defaults;
