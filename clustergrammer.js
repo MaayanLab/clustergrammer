3bd4935f7df0e4cf124b83b5e02ab66d812dfab9
Pavel Sidorenko <fat.cat.1703@gmail.com>
2022-12-13 12:42:16 +0100

    add tile over/out events

--- clustergrammer.js
+++ clustergrammer.js
@@ -24847,83 +24847,42 @@ var Clustergrammer =
 
 /***/ }),
 /* 98 */
-/***/ (function(module, exports, __webpack_require__) {
-
-	var underscore = __webpack_require__(3);
+/***/ (function(module, exports) {
 
 	module.exports = function mouseover_tile(params, inst_selection, tip, inst_arguments) {
-
 	  var inst_data = inst_arguments[0];
-	  var args = [].slice.call(inst_arguments);
-	  var timeout;
-	  var delay = 1000;
-
-	  d3.select(inst_selection).classed('hovering', true);
-
-	  underscore.each(['row', 'col'], function (inst_rc) {
-
-	    d3.selectAll(params.root + ' .' + inst_rc + '_label_group text').style('font-weight', function (d) {
+	  d3.select(inst_selection).classed("hovering", true);
+	  ["row", "col"].forEach(function (inst_rc) {
+	    d3.selectAll(params.root + " ." + inst_rc + "_label_group text").style("font-weight", function (d) {
 	      var font_weight;
-	      var inst_found = inst_data[inst_rc + '_name'].replace(/_/g, ' ') === d.name;
-
+	      var inst_found = inst_data[inst_rc + "_name"].replace(/_/g, " ") === d.name;
 	      if (inst_found) {
-	        font_weight = 'bold';
+	        font_weight = "bold";
 	      } else {
-	        font_weight = 'normal';
+	        font_weight = "normal";
 	      }
 	      return font_weight;
 	    });
 	  });
-
-	  args.push(inst_selection);
-	  clearTimeout(timeout);
-	  timeout = setTimeout(check_if_hovering, delay, inst_selection);
-
-	  function check_if_hovering() {
-
-	    if (d3.select(inst_selection).classed('hovering')) {
-
-	      var inst_zoom = Number(d3.select(params.root + ' .viz_svg').attr('is_zoom'));
-
-	      if (inst_zoom === 0) {
-
-	        if (params.matrix.show_tile_tooltips && tip !== null) {
-
-	          d3.selectAll(params.viz.root_tips + '_tile_tip').style('display', 'block');
-
-	          tip.show.apply(inst_selection, args);
-
-	          if (params.tile_tip_callback != null) {
-	            var tile_info = args[0];
-	            params.tile_tip_callback(tile_info);
-	          }
-	        }
-	      }
-	    }
-	  }
+	  dispatchEvent(new CustomEvent("TILE_MOUSEOVER", {
+	    detail: inst_arguments.__data__
+	  }));
 		};
 
 /***/ }),
 /* 99 */
-/***/ (function(module, exports, __webpack_require__) {
-
-	var underscore = __webpack_require__(3);
+/***/ (function(module, exports) {
 
 	module.exports = function mouseout_tile(params, inst_selection, tip) {
-
-	  d3.select(inst_selection).classed('hovering', false);
-
-	  d3.selectAll(params.viz.root_tips + '_tile_tip').style('display', 'none');
-
-	  underscore.each(['row', 'col'], function (inst_rc) {
-
-	    d3.selectAll(params.root + ' .' + inst_rc + '_label_group text').style('font-weight', 'normal');
+	  d3.select(inst_selection).classed("hovering", false);
+	  d3.selectAll(params.viz.root_tips + "_tile_tip").style("display", "none");
+	  ["row", "col"].forEach(function (inst_rc) {
+	    d3.selectAll(params.root + " ." + inst_rc + "_label_group text").style("font-weight", "normal");
 	  });
-
-	  if (tip != null) {
-	    tip.hide();
-	  }
-	};
+	  dispatchEvent(new CustomEvent("TILE_MOUSEOUT", {
+	    detail: inst_selection.__data__
+	  }));
+		};
 
 /***/ }),
 /* 100 */
