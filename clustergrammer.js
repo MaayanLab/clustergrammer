ec9ebafa3a23986719e01118e2513afd2895340c
Pavel Sidorenko <fat.cat.1703@gmail.com>
2022-12-21 17:53:01 +0100

    fix event

--- clustergrammer.js
+++ clustergrammer.js
@@ -13904,7 +13904,7 @@ var Clustergrammer =
 /***/ (function(module, exports) {
 
 	module.exports = function click_tile(inst_arguments) {
-	  dispatchEvent(new CustomEvent('TILE_MOUSEOVER', {
+	  dispatchEvent(new CustomEvent('TILE_CLICK', {
 	    detail: {
 	      tile: inst_arguments[0]
 	    }
--- clustergrammer.node.js
+++ clustergrammer.node.js
@@ -14157,7 +14157,7 @@ module.exports =
 	'use strict';
 
 	module.exports = function click_tile(inst_arguments) {
-	  dispatchEvent(new CustomEvent('TILE_MOUSEOVER', {
+	  dispatchEvent(new CustomEvent('TILE_CLICK', {
 	    detail: {
 	      tile: inst_arguments[0]
 	    }
